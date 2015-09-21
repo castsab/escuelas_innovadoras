491d59f0dbf9f225911958477d82c5c846210186
Diego Armando Castellanos Sabogal <castsabproyectos@gmail.com>
2015-09-21 16:09:50 -0500

    Commit de prueba

--- sites/all/modules/users_projects/js/usersProjectsController.js
+++ sites/all/modules/users_projects/js/usersProjectsController.js
@@ -1,13 +1,18 @@
-angular.module('UsersProjects', []).controller('usersProjectsController', function($scope) {
+angular.module('UsersProjects', []).controller('usersProjectsController', function($scope,$http) {
 
     $scope.users = [
         {'name': 'prueba', 'lastname': 'test'}
     ];
 
     $scope.add = function() {
-        var email = $scope.email;
-
-        alert('add...' + email);
+        
+        var data = {
+            'name': $scope.name,
+            'lastname': $scope.lastname,
+            'email': $scope.email
+        };
+        
+        //$http.post('../users_projects.module',data);
     };
 });
 
