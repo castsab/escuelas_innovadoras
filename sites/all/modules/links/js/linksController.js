da6c2cfdb8b5a55e13a88c28827943bde400f1a1
Diego Armando Castellanos Sabogal <castsabproyectos@gmail.com>
2015-09-25 15:01:10 -0500

    Se adiciona modulo para adicionar los enlaces al proyecto

--- sites/all/modules/links/js/linksController.js
+++ sites/all/modules/links/js/linksController.js
@@ -1,70 +1,8 @@
+alert('ttttttt');
+
 var app = "";
 
-app = angular.module('UsersProjects', []);
-
-app.controller('usersProjectsController', function($scope, $http) {
-    var pid = document.getElementById('pid').value;
-    var url = Drupal.settings.basePath + 'users_projects/listUsersByProject';
-    $http.post(url,{'pid': pid}).success(function(data) {
-        if (data.response == true)
-            $scope.users = data.arrayDatos;
-    }).error(function(data, status, headers, config) {
-        alert('<< Se produjo un error, favor intente más tarde. >>');
-    });
-
-    $scope.addUserByProject = function() {
-        var url = Drupal.settings.basePath + 'users_projects/addUserByProject';
-        $http.post(url, {'email': $scope.email,'pid':pid}).success(function(data) {
-            if (data.response == true){
-                $scope.users.push(data.arrayDatos);
-                $scope.typeAlert = 'status';
-                $scope.statusAlert = true;
-                $scope.msgAlert = 'Registro almacenado correctamente.';
-                $scope.email = "";
-            }else{
-                if(data.response == 2){
-                    $scope.typeAlert = 'warning';
-                    $scope.statusAlert = true;
-                    $scope.msgAlert = 'Este registro ya fue adicionado.';
-                    $scope.email = "";
-                }else{
-                    if(data.response == 3){
-                        $scope.typeAlert = 'warning';
-                        $scope.statusAlert = true;
-                        $scope.msgAlert = 'Este usuario no esta registrado.';
-                        $scope.email = "";
-                    }else{
-                        $scope.typeAlert = 'warning';
-                        $scope.statusAlert = true;
-                        $scope.msgAlert = 'Usted no tiene permisos para esta acción.';
-                        $scope.email = "";
-                    }
-                }
-            }
-        }).error(function(data, status, headers, config) {
-            alert('<< Se produjo un error, favor intente más tarde. >>');
-        });
-    };
-    
-    $scope.removeUserByProject = function(index,uid) {
-        var url = Drupal.settings.basePath + 'users_projects/removeUserByProject';
-        $http.post(url, {'uid': uid,'pid':pid}).success(function(data) {
-            if (data.response == true){
-                $scope.users.splice(index, 1);
-                $scope.typeAlert = 'status';
-                $scope.statusAlert = true;
-                $scope.msgAlert = 'Registro borrado correctamente.';
-            }else{
-                $scope.typeAlert = 'warning';
-                $scope.statusAlert = true;
-                $scope.msgAlert = 'Usted no tiene permisos para esta acción.';
-                $scope.email = "";
-            }
-        }).error(function(data, status, headers, config) {
-            alert('<< Se produjo un error, favor intente más tarde. >>');
-        });
-    };
-});
+app = angular.module('links', []);
 
 app.controller('linksController', function($scope, $http) {
     alert('fffffffffffffffffffffffff');
